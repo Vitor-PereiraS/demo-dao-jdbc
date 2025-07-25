a4d6f477645ccb2b1364f448370c19407dfe2670
Vitor-PereiraS <vitorpereirasou2010@gmail.com>
2025-07-25 14:05:45 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import endities.Department;
+import endities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 
         Department obj = new Department(1, "books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
 
     }
 }
