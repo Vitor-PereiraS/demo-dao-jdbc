4f674e6327aeaa04247159488f148098d0bdec96
Vitor-PereiraS <vitorpereirasou2010@gmail.com>
2025-07-25 14:42:21 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import endities.Department;
 import endities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 import java.util.Date;
 
@@ -13,6 +15,9 @@ public class Program {
         System.out.println(obj);
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.creatSellerDao();
+
         System.out.println(seller);
 
     }
